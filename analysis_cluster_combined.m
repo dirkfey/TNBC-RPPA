49d65e3c8806ca65e28f6dc6d6c297cf49eb6c66
Dirk Fey <dirk.fey@ucd.ie>
2022-02-11 12:22:31 +0000

    Tidied up cluster analysis

--- analysis_cluster_combined.m
+++ analysis_cluster_combined.m
@@ -1,12 +1,12 @@
 
-
 %% Clustergramms for all drugs - Heatmaps
+%  Requires: dataFoldChangewIC50 as variable in workspace
+
+% Select drugs and analytes and pre-process data:
 
 myDrugs = ["AICAR";"CHIR-98014";"Cisplatin";"Dorsomorphin";"Everolimus";"Ipatasertib";"PF-00562271";"PF-4708671";"QNZ";"Stattic";"TAK-632";"U0126"];
-%myDrugs = ["AICAR";"CHIR-98014";"Ipatasertib"];
 
 TF = contains(dataFoldChangewIC50.Treatment, myDrugs);
-mySelection0 = dataFoldChange_allwIC50(TF,:);
 mySelection0 = dataFoldChangewIC50(TF,:);
 mySelection0{:,3:end-1} = log2(mySelection0{:,3:end-1});
 xaxis = 'IC50';
@@ -27,8 +27,7 @@ myAnalytes = {'AKT_S473_' 'AKT_T308_' 'AMPK_T172_' 'cRAF_S338_' 'GSK3B_S9_' 'MAP
 mask = ismember(mySelection0.Properties.VariableNames, [myAnalytes 'CellLine' 'Treatment', 'IC50']);
 mySelection0(:,~mask) = [];
 
-%%
-%Convert to DataMatrix and cluster
+%% Convert to DataMatrix and cluster
 import bioma.data.*
 mySelection00 = mySelection0;
 rownames = join([mySelection00.CellLine, mySelection00.Treatment],'::')
@@ -41,22 +40,11 @@ s = struct('Labels', rownames, 'Colors', colordefine);
 myDM = DataMatrix(mySelection00{:,3:end},'RowNames', rownames,'ColNames', mySelection00.Properties.VariableNames(3:end) )
 cgo = clustergram(myDM(:,1:(end-1)), 'cluster', 2, 'Colormap', 'redbluecmap')
 %set(cgo, 'RowLabels', rownames, 'LabelsWithMarkers', 1, 'RowLabelsColor', s)
-th = addTitle(cgo, myDrug)
-%set(cgo, 'Fontsize', 10)
-figure(3), clf
-fh = plot(cgo,gcf);
-%ha = get(fh, 'Children'); ha = ha(2);
-%set(fh, 'Position', [apos(1) 0.3 apos(3), 0.7*apos(4)])
-%fh.Title.String = myDrug;
-%tpos = 8;
-%fh.Title.Position(2)=1.4*tpos;
-%print(gcf, ['../figures/cluster_2_' char(myDrug) '.png'], '-dpng', '-r600')
 
-apos = get(fh, 'Position')
 
-%%
+%% Draw individual heatmap plots for each drug
 i=1;
-for i = 1%1:length(myDrugs)
+for i = 1:length(myDrugs)
 figure(3), clf
 th = addTitle(cgo, myDrugs(i))
 th.String = myDrugs(i)
@@ -64,6 +52,7 @@ fh = plot(cgo,gcf);
 box on
 fpos = get(gcf, 'Pos');
 set(gcf, 'Pos', [fpos(1:2) 560 300])
+% Zoom into region of the the selectred drug:
 set(fh, 'ylim', (i-1)*7+[0.5 7.5])
 set(fh, 'Position', [apos(1) 0.37 apos(3) apos(4)+apos(2)-0.37])
 set(fh, 'Fontsize', 10)
